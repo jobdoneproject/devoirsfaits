4428aef37cfd8f75536317910733f58c69b42252
Paul Ferreira Silva <paul.ferreirasilva@wildcodeschool.fr>
2018-05-14 10:51:35 +0200

    War packaging

--- src/main/java/fr/educ/devoirsfaits/DevoirsfaitsApplication.java
+++ src/main/java/fr/educ/devoirsfaits/DevoirsfaitsApplication.java
@@ -2,11 +2,12 @@ package fr.educ.devoirsfaits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class DevoirsfaitsApplication {
+public class DevoirsfaitsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevoirsfaitsApplication.class, args);
