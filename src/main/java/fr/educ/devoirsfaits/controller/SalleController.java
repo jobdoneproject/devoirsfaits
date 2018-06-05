48d293272e89748f48ad354a24c8147db6d43c0e
laurent bernabé <Laurent_Bernabe_student2018@wilder.school>
2018-06-05 10:08:36 +0200

    Possibilité d'obtenir une salle par id établissement et par id salle.

--- src/main/java/fr/educ/devoirsfaits/controller/SalleController.java
+++ src/main/java/fr/educ/devoirsfaits/controller/SalleController.java
@@ -5,9 +5,10 @@ import fr.educ.devoirsfaits.service.SalleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/etablissement")
+@RequestMapping("/etablissements")
 @CrossOrigin(origins = {"*"}, maxAge = 4800, allowCredentials = "false")
 public class SalleController {
 
@@ -18,6 +19,15 @@ public class SalleController {
     @GetMapping("/{id}/salles")
     public List<Salle> getAllByEtablissement(@PathVariable(value = "id") long idEtablissement) {
         return salleService.findAllByIdEtablissement(idEtablissement);
+    }
+
+    @GetMapping("/{idEtabl}/salles/{idSalle}")
+    public Salle getByEtablissementAndByClassroomId(
+            @PathVariable(value = "idEtabl") long idEtablissement,
+            @PathVariable(value = "idSalle") long idSalle
+    ) {
+        List<Salle> sallesEtablissement = salleService.findAllByIdEtablissement(idEtablissement);
+        return sallesEtablissement.stream().filter(salleCourante -> salleCourante.getIdSalle() == idSalle).limit(1).collect(Collectors.toList()).get(0);
     }
 
 }
