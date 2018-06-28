9481b8cf407efc35621ae8e640e374eada9b5354
richard <richard@zaclys.net>
2018-06-28 09:36:52 +0200

    update clevercloud war.json and deploy

--- clevercloud/build.sh
+++ clevercloud/build.sh
@@ -2,7 +2,9 @@
 echo "Copy deployment War file is started"
 DEVOIRS_WAR=`ls target/*.war|grep war`
 timestp = date -j -f "%m/%d/%y %H:%M:%S %p" now +"%m%d%y_%H%M%S"
-$DEVOIRS_WAR_DEST = devoirsfaits_$timestp.war
+ $DEVOIRS_WAR_DEST = devoirsfaits_$timestp.war
+touch $DEVOIRS_WAR_DEST
+chmod 755 $DEVOIRS_WAR_DEST
 #curl -L http://mirrors.jenkins.io/war/latest/jenkins.war --output jenkins.war
 echo "Configured CC_FS_BUCKET folder is : " {$CC_FS_BUCKET}
 echo "...Copy " $DEVOIRS_WAR " to CC_FS_BUCKET folder as " $DEVOIRS_WAR_DEST
